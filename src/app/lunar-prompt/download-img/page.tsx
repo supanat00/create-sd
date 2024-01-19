4b5dc0d5c927eeccb3fde4ba086fe4a3375b1ccc
supanat00 <supanat90@gmail.com>
2024-01-19 19:35:18 +0700

    ui-test-02

--- src/app/lunar-prompt/download-img/page.tsx
+++ src/app/lunar-prompt/download-img/page.tsx
@@ -2,6 +2,7 @@
 "use client"
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import styles from "../../styles.module.css";
 
 const Loading: React.FC = () => {
     const [loading, setLoading] = useState(true);
@@ -33,64 +34,71 @@ const Loading: React.FC = () => {
         fetchImageData();
     }, []); // The empty dependency array ensures that this effect runs only once when the component mounts
 
-    useEffect(() => {
-        const handleDownload = async () => {
-            try {
-                if (cloudinaryUrl) {
-                    // Fetch the image data from Cloudinary
-                    const response = await fetch(cloudinaryUrl);
+    // useEffect(() => {
+    //     const handleDownload = async () => {
+    //         try {
+    //             if (cloudinaryUrl) {
+    //                 // Fetch the image data from Cloudinary
+    //                 const response = await fetch(cloudinaryUrl);
 
-                    if (response.ok) {
-                        // Convert the response to a Blob
-                        const blob = await response.blob();
+    //                 if (response.ok) {
+    //                     // Convert the response to a Blob
+    //                     const blob = await response.blob();
 
-                        // Create a URL for the Blob
-                        const blobUrl = URL.createObjectURL(blob);
+    //                     // Create a URL for the Blob
+    //                     const blobUrl = URL.createObjectURL(blob);
 
-                        // Create a download link
-                        const downloadLink = document.createElement('a');
-                        downloadLink.href = blobUrl;
-                        downloadLink.download = 'wallpaper_theme_lunarday.png';
+    //                     // Create a download link
+    //                     const downloadLink = document.createElement('a');
+    //                     downloadLink.href = blobUrl;
+    //                     downloadLink.download = 'wallpaper_theme_lunarday.png';
 
-                        // Click the link to trigger the download
-                        downloadLink.click();
-                    } else {
-                        console.error('Error fetching image:', response.statusText);
-                    }
-                }
-            } catch (error) {
-                console.error('Error downloading image:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
+    //                     // Click the link to trigger the download
+    //                     downloadLink.click();
+    //                 } else {
+    //                     console.error('Error fetching image:', response.statusText);
+    //                 }
+    //             }
+    //         } catch (error) {
+    //             console.error('Error downloading image:', error);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
 
-        // Trigger download when the image is loaded
-        if (!loading && cloudinaryUrl) {
-            handleDownload();
-        }
-    }, [loading, cloudinaryUrl]);
+    //     // Trigger download when the image is loaded
+    //     if (!loading && cloudinaryUrl) {
+    //         handleDownload();
+    //     }
+    // }, [loading, cloudinaryUrl]);
 
     return (
-        <main className="mt-8 text-center max-w-screen-xl mx-auto flex justify-center items-center">
-            <div className="flex-1">
+        <div className={styles.bgWrap}>
+            <div className={`absolute h-screen w-screen`}>
                 {loading ? (
-                    // แสดง Loader หรือข้อความ "Loading..."
-                    <div className="rounded-lg shadow-md mb-8" style={{ width: 450, height: 100, backgroundColor: 'lightgray' }}>
-                        Loading...
+                    // {/* // แสดง Loader หรือข้อความ "Loading..." */}
+                    <div className={`absolute left-1/2 bottom-1/2 transform -translate-x-1/2`}>
+                        <div className="loader">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
                     </div>
                 ) : cloudinaryUrl ? (
                     // แสดงรูปภาพเมื่อโหลดเสร็จสิ้น
-                    <Image
-                        src={cloudinaryUrl}
-                        alt="Generated Image"
-                        className="rounded-lg shadow-md mb-8"
-                        width={450}
-                        height={100}
-                    />
+                    <div className={`absolute left-1/2  transform -translate-x-1/2 top-1/2 -translate-y-1/2`}>
+                        <Image
+                            src={cloudinaryUrl}
+                            alt="Generated Image"
+                            className={`rounded-lg shadow-md`}
+                            width={450}
+                            height={100}
+                        />
+                    </div>
                 ) : null}
             </div>
-        </main>
+        </div>
     );
 };
 
