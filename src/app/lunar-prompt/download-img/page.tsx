edb500c3b3bf4a7416776d79bcec48aae56d9ad8
supanat00 <supanat90@gmail.com>
2024-01-19 22:26:41 +0700

    update-loading

--- src/app/lunar-prompt/download-img/page.tsx
+++ src/app/lunar-prompt/download-img/page.tsx
@@ -13,7 +13,7 @@ const Loading: React.FC = () => {
                 const response = await fetch(
                     '/api/stable-diffusion',
                     {
-                        next: { revalidate: 0 }
+                        next: { revalidate: 2 }
                     }
                 );
                 const imageData = await response.json();
@@ -33,51 +33,56 @@ const Loading: React.FC = () => {
         fetchImageData();
     }, []); // The empty dependency array ensures that this effect runs only once when the component mounts
 
-    useEffect(() => {
-        const handleDownload = async () => {
-            try {
-                if (cloudinaryUrl) {
-                    // Fetch the image data from Cloudinary
-                    const response = await fetch(cloudinaryUrl);
+    // useEffect(() => {
+    //     const handleDownload = async () => {
+    //         try {
+    //             if (cloudinaryUrl) {
+    //                 // Fetch the image data from Cloudinary
+    //                 const response = await fetch(cloudinaryUrl);
 
-                    if (response.ok) {
-                        // Convert the response to a Blob
-                        const blob = await response.blob();
+    //                 if (response.ok) {
+    //                     // Convert the response to a Blob
+    //                     const blob = await response.blob();
 
-                        // Create a URL for the Blob
-                        const blobUrl = URL.createObjectURL(blob);
+    //                     // Create a URL for the Blob
+    //                     const blobUrl = URL.createObjectURL(blob);
 
-                        // Create a download link
-                        const downloadLink = document.createElement('a');
-                        downloadLink.href = blobUrl;
-                        downloadLink.download = 'wallpaper_theme_lunarday.png';
+    //                     // Create a download link
+    //                     const downloadLink = document.createElement('a');
+    //                     downloadLink.href = blobUrl;
+    //                     downloadLink.download = 'wallpaper_theme_lunarday.png';
 
-                        // Click the link to trigger the download
-                        downloadLink.click();
-                    } else {
-                        console.error('Error fetching image:', response.statusText);
-                    }
-                }
-            } catch (error) {
-                console.error('Error downloading image:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
+    //                     // Click the link to trigger the download
+    //                     downloadLink.click();
+    //                 } else {
+    //                     console.error('Error fetching image:', response.statusText);
+    //                 }
+    //             }
+    //         } catch (error) {
+    //             console.error('Error downloading image:', error);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
 
-        // Trigger download when the image is loaded
-        if (!loading && cloudinaryUrl) {
-            handleDownload();
-        }
-    }, [loading, cloudinaryUrl]);
+    //     // Trigger download when the image is loaded
+    //     if (!loading && cloudinaryUrl) {
+    //         handleDownload();
+    //     }
+    // }, [loading, cloudinaryUrl]);
 
     return (
-        <main className="mt-8 text-center max-w-screen-xl mx-auto flex justify-center items-center">
+        <main className="mt-8 max-w-screen-xl mx-auto flex justify-center items-center">
             <div className="flex-1">
                 {loading ? (
                     // แสดง Loader หรือข้อความ "Loading..."
-                    <div className="rounded-lg shadow-md mb-8" style={{ width: 450, height: 100, backgroundColor: 'lightgray' }}>
-                        Loading...
+                    <div className={`absolute top-80`}>
+                        <div className="loader">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
                     </div>
                 ) : cloudinaryUrl ? (
                     // แสดงรูปภาพเมื่อโหลดเสร็จสิ้น
