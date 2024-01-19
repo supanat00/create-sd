29803665e01cae46e6d0a0f2370b717bc1d4119e
supanat00 <supanat90@gmail.com>
2024-01-20 04:45:41 +0700

    update-texts

--- src/app/lunar-prompt/show-pic/page.tsx
+++ src/app/lunar-prompt/show-pic/page.tsx
@@ -143,8 +143,9 @@ export default function Page() {
                 {/* qr-bg และ qr-img */}
                 {!showButtons && (
                     <>
+
                         <Image
-                            alt="Center Button Image"
+                            alt="frame"
                             src={qr}
                             width={690}
                             height={250}
@@ -159,7 +160,11 @@ export default function Page() {
                                 height={300}
                             />
                         </div>
-                        <div className={`absolute top-96 mt-80 right-32`}>
+                        <div className={`absolute top-10 right-32`}>
+                            <h1 className="text-center text-white text-5xl font-semi">สแกนเพื่อดาวน์โหลด<br></br>ภาพ Wallpaper</h1>
+                        </div>
+                        <div className={`absolute bottom-20 right-32`}>
+
                             <div
                                 className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
                             </div>
