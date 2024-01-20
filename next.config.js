18fa8a04a56c59e28eb5d945c41a0630a2d5efcd
supanat00 <supanat90@gmail.com>
2024-01-21 00:35:17 +0700

    build-file-data

--- next.config.js
+++ next.config.js
@@ -12,13 +12,3 @@ module.exports = {
     ],
   },
 };
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
-}
